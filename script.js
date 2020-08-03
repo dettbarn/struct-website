39cd9f0aa8926f578abd1e72daed2a0de278590f
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2020-08-03 10:21:10 +0200

    Highlight current page in navigation.

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+$(function() {
+    // this will get the full URL at the address bar
+    var url = window.location.href;
+    
+    // for current page set navigation li to active	
+    $("#navigation ul li a").each(function() {
+        if (url == (this.href) || url + "index.html" == (this.href)) {
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
