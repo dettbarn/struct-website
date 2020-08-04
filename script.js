6f79db83243b8e7907c687fa8e09f78edf83b4d1
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2020-08-04 23:51:50 +0200

    Use nav tag.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(function() {
     var url = window.location.href;
     
     // for current page set navigation li to active	
-    $("#navigation ul li a").each(function() {
+    $("nav ul li a").each(function() {
         if (url == (this.href) || url + "index.html" == (this.href) || url + "/index.html" == (this.href)) {
             $(this).closest("li").addClass("active");
         }
