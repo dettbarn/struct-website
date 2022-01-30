04b2b7bf8a37f3a1778c7329021093e6e17e0452
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2022-01-30 14:48:55 +0100

    code style

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function() {
     // this will get the full URL at the address bar
     var url = window.location.href;
     
-    // for current page set navigation li to active	
+    // for current page set navigation li to active
     $("nav ul li a").each(function() {
         if (url == (this.href) || url + "index.html" == (this.href) || url + "/index.html" == (this.href)) {
             $(this).addClass("active");
